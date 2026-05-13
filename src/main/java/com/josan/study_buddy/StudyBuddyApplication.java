857583254163f1cbd924a2e4bf668af55d80848d
josan <sanchezjosan111@gmail.com>
2026-05-12 22:28:35 -0700

    Initial commit

---
+++ src/main/java/com/josan/study_buddy/StudyBuddyApplication.java
@@ -0,0 +1,13 @@
+package com.josan.study_buddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyBuddyApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(StudyBuddyApplication.class, args);
+	}
+
+}
