420fc7f36c5c0a94c067ecd34197462fcad0b16e
josan <sanchezjosan111@gmail.com>
2026-05-22 20:31:07 -0700

    Add DTO for subject response

--- src/main/java/com/josan/study_buddy/Subject/SubjectDto/GenericSubjectResponse.java
+++ src/main/java/com/josan/study_buddy/Subject/SubjectDto/GenericSubjectResponse.java
@@ -3,7 +3,6 @@ package com.josan.study_buddy.Subject.SubjectDto;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.*;
-import tools.jackson.databind.ext.QNameSerializer;
 
 @Getter
 @Setter
