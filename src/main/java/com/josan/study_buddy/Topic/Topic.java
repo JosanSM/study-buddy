077c2d1c2b6c763731378a6fb0f46b24d3f880c9
josan <sanchezjosan111@gmail.com>
2026-05-28 11:39:13 -0700

    Simplify Topic entity class using Lombok

--- src/main/java/com/josan/study_buddy/Topic/Topic.java
+++ src/main/java/com/josan/study_buddy/Topic/Topic.java
@@ -4,8 +4,14 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.josan.study_buddy.Subject.Subject;
 import com.josan.study_buddy.User.User;
 import jakarta.persistence.*;
+import lombok.*;
 
 @Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class Topic {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -22,52 +28,4 @@ public class Topic {
     @ManyToOne
     @JoinColumn(name = "subject_id")
     private Subject subject;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getNotes() {
-        return notes;
-    }
-
-    public void setNotes(String notes) {
-        this.notes = notes;
-    }
-
-    public String getTopicStatus() {
-        return topicStatus;
-    }
-
-    public void setTopicStatus(String topicStatus) {
-        this.topicStatus = topicStatus;
-    }
-
-    public User getUser() {
-        return user;
-    }
-
-    public void setUser(User user) {
-        this.user = user;
-    }
-
-    public Subject getSubject() {
-        return subject;
-    }
-
-    public void setSubject(Subject subject) {
-        this.subject = subject;
-    }
 }
