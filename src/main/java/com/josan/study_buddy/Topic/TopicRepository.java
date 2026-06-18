270bd21a1f3a01a5a1735dd2227de04ade076dfa
josan <sanchezjosan111@gmail.com>
2026-06-17 21:59:02 -0700

    Add due-topics query to TopicRepository

--- src/main/java/com/josan/study_buddy/Topic/TopicRepository.java
+++ src/main/java/com/josan/study_buddy/Topic/TopicRepository.java
@@ -3,8 +3,12 @@ package com.josan.study_buddy.Topic;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 @Repository
 public interface TopicRepository extends JpaRepository<Topic, Long> {
     boolean existsByTitleAndSubjectId(String title, Long subjectId);
     boolean existsByTitleAndSubjectIdAndIdNot(String title, Long subjectId, Long id);
+    List<Topic> findByUserIdAndNextReviewAtLessThanEqual(Long userId, LocalDate date);
 }
